a434e55056a0168ff3ae391c1212c97a51cb65d3
Benny Degezelle <hi@monkeypatch.be>
2013-01-15 03:05:19 +0100

    Update will_paginate

--- lib/radiant/pagination/link_renderer.rb
+++ lib/radiant/pagination/link_renderer.rb
@@ -1,10 +1,10 @@
 # This handy simplification is adapted from SphinxSearch (thanks)
 # and originally came from Ultrasphinx
 # it saves us a lot of including and bodging to make will_paginate's template calls work in the Page model
-
+require 'will_paginate/view_helpers/action_view'
 module Radiant
   module Pagination
-    class LinkRenderer < WillPaginate::LinkRenderer
+    class LinkRenderer < WillPaginate::ActionView::LinkRenderer
       def initialize(url_stem)
         @url_stem = url_stem
       end
